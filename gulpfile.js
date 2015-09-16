80341e6c63a9e899e69dc705c640be983165459a
Nino Seyyedi <nino.seyyedi@gmail.com>
2015-09-16 22:33:09 +0200

    working on gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var del = require('del');
+var zip = require('gulp-zip');
 var exec = require('child_process').exec;
 var path = require('path');
 
 var paths = {
+	code: 'D:\\Seyyedi\\Code\\Seyyedi.RemoteMpc',
 	build: 'D:\\Seyyedi\\Code\\Seyyedi.RemoteMpc\\bin',
+	apps: 'D:\\Seyyedi\\Applications',
 	app: 'D:\\Seyyedi\\Applications\\Seyyedi.RemoteMpc',
 	msbuild: 'C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe'
 };
@@ -34,6 +37,30 @@ gulp.task('build', function(cb) {
 });
 
 gulp.task('rebuild', ['clean-build', 'build']);
+
+gulp.task('release', ['rebuild'], function(cb) {
+	var info = {
+		branch: '',
+		commitCount: 0
+	};
+	
+	exec('git rev-parse --abbrev-ref HEAD', function(err, stdout, stderr) {
+		if (err) cb(err);
+		info.branch = stdout.trim();
+		
+		exec('git rev-list HEAD --count', function(err, stdout, stderr) {
+			if (err) cb(err);
+
+			info.commitCount = stdout.trim();
+			
+			gulp
+				.src(path.join(paths.build, 'Release', '**', '*'))
+				.pipe(zip('Seyyedi.RemoteMpc-' + info.branch + '-' + info.commitCount + '.zip'))
+				.pipe(gulp.dest(paths.apps))
+				.on('end', cb);
+		});
+	});
+});
 
 gulp.task('deploy', ['rebuild', 'clean-app'], function() {
 	return gulp
